95094f99636c37fc569eae49185637330c6e5023
jack2522 <91106293+jack2522@users.noreply.github.com>
2022-03-22 23:01:54 +0530

    Add files via upload

---
+++ ip.py
@@ -0,0 +1,21 @@
+import LIST
+from LIST.id import *
+from LIST.it import *
+from LIST.jp import *
+from LIST.us import *
+from LIST.fr import *
+from LIST.kr import *
+from LIST.de import *
+from LIST.tr import *
+import requests,re,os
+
+b="\033[0;34m"
+g="\033[1;32m"
+w="\033[1;37m"
+r="\033[1;31m"
+y="\033[1;33m"
+cyan = "\033[0;36m"
+lgray = "\033[0;37m"
+dgray = "\033[1;30m"
+ir = "\033[0;101m"
+reset = "\033[0m"
